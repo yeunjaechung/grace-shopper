3ef2af2e9d02298eb6b1cf9a3a7f1880713f3ce4
Ryan Scoville <ryanscoville29@yahoo.com>
2022-08-03 11:52:17 -0500

    refactored our models

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 //this is the access point for all things database related!
-
+const Sequelize = require('sequelize')
 const db = require('./db')
 
 const User = require('./models/User')
@@ -7,11 +7,33 @@ const Product = require('./models/Product')
 const Order = require('./models/Order')
 //associations could go here!
 
+const Order_Products = db.define('Order_Product', {
+quantity: {
+  type: Sequelize.INTEGER
+},
+//Price is calculated in pennies
+unitPrice: {
+  type: Sequelize.INTEGER
+},
+totalPrice: {
+  type: Sequelize.INTEGER
+}
+})
+
+
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
+Product.belongsToMany(Order, {through: Order_Products});
+Order.belongsToMany(Product, {through: Order_Products});
+
 module.exports = {
   db,
   models: {
     User,
     Product,
-    Order
+    Order,
+    Order_Products
   },
 }
--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -2,28 +2,10 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate:{
-      isEmail: true
-    }
-  },
-  phoneNumber:{
-    type: Sequelize.STRING,
-    validate: {
-      isNumeric: true
-    }
-  },
-  shippingAddress: {
-    type: Sequelize.STRING
-  },
-  billingAddress: {
-    type: Sequelize.STRING
-  },
-  // paymentInfo: {
-  //   //Look into Stripe / PayPal / Whatever at a later time
-  // }
-})
+  status: {
+    type: Sequelize.ENUM('open', 'closed'),
+    defaultValue: 'open'
+  }
+});
 
-module.exports = Order
+
