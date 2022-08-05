72f7480f94c2516f1fd554c1b0235c2b7abfbb8f
Ryan Scoville <85287770+rscoville29@users.noreply.github.com>
2022-08-05 15:19:36 -0500

    Ryan express routes (#38)
    * refactored our models
    
    * changed the name of our project
    
    * refactored the viewcart after studying sequelize
    
    * completed express routes
    
    * Update users.js
    
    consolidated imported models
    
    * Update users.js
    
    removed broken remove item from cart route
    
    Co-authored-by: Warren Au <onsenkame@gmail.com>

--- server/api/products.js
+++ server/api/products.js
@@ -9,6 +9,18 @@ router.get('/', async (req, res, next) => {
     } catch(err){
         next(err);
     }
+});
+
+//add a route for single product view
+
+router.get('/:productId', async (req, res, next) => {
+    try{
+        const {productId} = req.params;
+        const product = await Product.findByPk(productId);
+        res.send(product);
+    }catch(err){
+        next(err);
+    }
 })
 
 
