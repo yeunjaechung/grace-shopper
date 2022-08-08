a9723371f3001300463a00b524ca329ca5076ff1
Ryan Scoville <85287770+rscoville29@users.noreply.github.com>
2022-08-08 17:02:11 -0500

    Ryan single pokemon (#56)
    * refactored our models
    
    * changed the name of our project
    
    * heroku specs added
    
    * added seed script to procfile
    
    * trying db configs
    
    * procfile config
    
    * changed web to worker
    
    * further config
    
    * changed class functions to prototype for troubleshooting. error indicates: Error: Incorrect email/password at Function.User.authenticate (/app/server/db/models/User.js:71:19)
    
    * changed the procfile
    
    * changed exact path
    
    * removed jwt from json
    
    * added to gitignore
    
    * removed package.json from gitignore
    
    * created basic single page, added store actions, need to import thunk and button
    
    * added router delete route
    
    * successfully deletes a product as admin
    
    * refactored deleted pokemonn
    
    Co-authored-by: Warren Au <onsenkame@gmail.com>

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,19 +1,63 @@
 import React from "react";
 import { connect } from "react-redux";
-
-import { fetchProduct } from "../store/singleProduct";
+import { fetchProduct, deleteProduct } from "../store/singleProduct";
 import { addItem } from "../store/order";
-
 import { addToGuestCart } from "../store/order";
 
 
 export class SingleProduct extends React.Component {
+  constructor(){
+    super();
+    // this.state = {
+    //   auth: {
+    //     userType: ''
+    //   }
+    // }
+    this.isAdmin = this.isAdmin.bind(this);
+  }
   componentDidMount() {
     const productId = this.props.match.params.productId;
     this.props.fetchProduct(productId);
   }
+  // componentDidUpdate(){
+  //   this.setState({userType: this.state.auth.userType});
+  // }
+  isAdmin(userType){
+    return userType === 'admin' ? true : false;
+  }
   render() {
+    // let userType = this.state.auth.userType || '';
+    console.log('THIS props in render', this.props);
     const product = this.props.product;
+
+    if(!product){
+      return <div>Pokemon Deleted! Go back to all products...
+      </div>
+    }
+    if(this.props.user.userType === 'admin'){  
+      return (
+        <div>
+          <img src={product.imageSmall}></img>
+          <h1>Product Name: {product.name}</h1>
+          <h2>{product.price}</h2>
+          <button type='button' onClick={() => this.props.addItem(product)}>Add to Cart</button>
+          <button type='button' onClick={() => this.props.deleteProduct(product)}>DELETE ITEM FROM DATABASE</button>
+        </div>
+      )
+    }else{
+      return (
+        <div>
+          <img src={product.imageSmall}></img>
+          <h1>Product Name: {product.name}</h1>
+          <h2>{product.price}</h2>
+          <button type='button' onClick={() => this.props.addItem(product)}>Add to Cart</button>
+        </div>
+      )
+    }
+    
+   ;}}
+/* needs to be resolved when final singleProduct Component!!!!!!!!!!!!!!!!!!!!!!
+this was the main:
     const isLoggedIn = this.props.isLoggedIn
 
     return (
@@ -35,10 +79,12 @@ export class SingleProduct extends React.Component {
     )
   }
 }
+*/
 
 const mapState = (state) => {
   return {
     product: state.product,
+    user: state.auth
     isLoggedIn: !!state.auth.id
   };
 };
@@ -47,6 +93,7 @@ const mapDispatch = (dispatch) => {
   return {
     fetchProduct: (productId) => dispatch(fetchProduct(productId)),
     addItem: (product) => dispatch(addItem(product)),
+    deleteProduct: (product) => dispatch(deleteProduct(product))
     addToGuestCart: (guestCart) => dispatch(addToGuestCart(guestCart))
   };
 };
--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -4,12 +4,26 @@ const TOKEN = "token";
 
 // ACTIONS
 const FETCH_SINGLE_PRODUCT = "FETCH_SINGLE_PRODUCT";
+const ADD_ITEM = "ADD_ITEM";
+const DELETE_ITEM = 'DELETE_ITEM';
 
 // ACTION CREATORS
 const setProduct = (product) => ({
   type: FETCH_SINGLE_PRODUCT,
   product,
 });
+
+const _addItem = (product) => ({
+  type: ADD_ITEM,
+  product,
+});
+
+const deleteItem = (product) => {
+  return {
+    type: DELETE_ITEM,
+    product
+  }
+}
 
 // THUNKS
 export const fetchProduct = (productId) => {
@@ -19,14 +33,40 @@ export const fetchProduct = (productId) => {
   };
 };
 
+export const deleteProduct = (product) => {
+  return async (dispatch) => {
+    const {data: deletedProduct} = await axios.delete(`/api/products/${product.id}`);
+    dispatch(deleteItem(deletedProduct));
+  }
+}
+
+export const addItem = (product) => {
+  const token = window.localStorage.getItem(TOKEN);
+  return async function (dispatch) {
+    const response = await axios.post(
+      `/api/users/addToCart`,
+      product,
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+  
+    // dispatch(_addItem(newItem));
+  };
+};
+
 const initialState = {};
 
 export default function singleProductReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_SINGLE_PRODUCT:
       return action.product;
-    // case ADD_ITEM:
-    //   return action.product;
+    case ADD_ITEM:
+      return action.product;
+    case DELETE_ITEM:
+        return null;
     default:
       return state;
   }
