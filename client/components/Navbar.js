312e28e92478984da8a5df620423ef247f405c2d
eln123 <98172322+eln123@users.noreply.github.com>
2022-08-10 10:11:07 -0400

    need CSS for all users (#79)

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
 
-const Navbar = ({ handleClick, isLoggedIn, cart, products }) => {
+const Navbar = ({ handleClick, isLoggedIn, cart, products, isAdmin }) => {
   let items = cart.products || [];
   let cartItems = items.reduce(function (accum, obj) {
     const {
@@ -18,7 +18,35 @@ const Navbar = ({ handleClick, isLoggedIn, cart, products }) => {
       <nav>
       <h1 className="anchor-container">GS: Pokemon TCG Shop</h1>
         {isLoggedIn ? (
-          <div>
+          isAdmin ? (<div>
+            {/* The navbar will show these links after you log in */}
+            <div className="linkHolder">
+              <div className="anchor-container">
+                <Link to="/home">Home</Link>
+              </div>
+              <div className="anchor-container">
+                <Link to="/products">Products({productsLength})</Link>
+              </div>
+              <div className="anchor-container">
+                <Link to="/cart">Cart({cartItems})</Link>
+              </div>
+              <div className="anchor-container">
+                <Link to="/editInfo">Edit Your Info</Link>
+              </div>
+
+              <div className="anchor-container">
+                <Link to="/users">View Users</Link>
+              </div>
+              <div className="anchor-container">
+                <a href="#" onClick={handleClick}>
+                  Logout
+                </a>
+              </div>
+            </div>
+          </div>)
+
+          :
+          (<div>
             {/* The navbar will show these links after you log in */}
             <div className="linkHolder">
               <div className="anchor-container">
@@ -39,7 +67,7 @@ const Navbar = ({ handleClick, isLoggedIn, cart, products }) => {
                 </a>
               </div>
             </div>
-          </div>
+          </div>)
         ) : (
           <div>
             {/* The navbar will show these links before you log in */}
@@ -70,6 +98,7 @@ const Navbar = ({ handleClick, isLoggedIn, cart, products }) => {
 const mapState = (state) => {
   return {
     isLoggedIn: !!state.auth.id,
+    isAdmin: state.auth.userType === "admin",
     cart: state.cart,
     products: state.products,
   };
